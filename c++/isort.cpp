c86411176a50a3e6e1c09a0ae60291ddd632b4f5
Morgan Baron <morgan.baron@rfcapitaladvisors.com>
2017-06-06 11:27:46 -0400

    fix isort, adding gtest

--- c++/isort.cpp
+++ c++/isort.cpp
@@ -1,31 +1,35 @@
 #include <array>
 #include <iostream>
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+template<size_t SIZE>
+void print(const std::array<int, SIZE>& arr) {
+    for (auto& el : arr) {
+        std::cout << el << std::endl;
+    }
+}
 
 template <size_t SIZE> void isort(std::array<int, SIZE> &arr) {
   // sorted contains single element
   auto sz = arr.size();
   for (auto i = 0; i < sz - 1; i++) {
-    // compare with unsorted and insert
-
-    // working backwards compare arr[j] with each membor of unsorted
-
     auto j = i + 1;
     while (arr[j] < arr[j - 1] && j >= 1) {
-      // swap
-      auto tmp = arr[j];
-      arr[j] = arr[j - 1];
-      arr[j - i] = tmp;
+      std::swap(arr[j-1], arr[j]);
 
       --j;
     }
   }
 }
 
-int main(int argc, char **argv) {
-  std::array<int, 5> arr = {5, 8, 9, 7, 6};
-  isort(arr);
+TEST(IsortTest, UnsortedPositive) {
+    std::array<int, 5> arr = {5, 8, 9, 7, 6};
+    isort(arr);
+    ASSERT_THAT(arr, ::testing::ElementsAre(5,6,7,8,9));
+}
 
-  for (auto& el : arr) {
-      std::cout << el << std::endl;
-  }
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS(); 
 }
